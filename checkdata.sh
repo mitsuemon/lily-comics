869bebcab68f35138fdd052b44d915cfd85ee00b
mitsuemon <chitose@kyouhakanojogainaikara.local>
2022-02-27 22:57:56 +0900

    Add print diff data

--- checkdata.sh
+++ checkdata.sh
@@ -4,3 +4,9 @@ while read line
 do
 	awk -F [,] '{if (NF <=10) printf("%d:%s",NR,$0)}' var=$line
 done < data.csv
+
+tmpfile="/tmp/checkdata.sh.$$"
+
+git diff > ${tmpfile}
+grep '^+9784' ${tmpfile} | awk -F , '{printf("- %s\n",$4)}'
+rm ${tmpfile}
